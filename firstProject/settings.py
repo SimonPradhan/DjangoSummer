f2b978dc026568f6d12828b921922a88a72eae33
SimonPradhan <simon234pradhan@gmail.com>
2023-08-16 08:33:56 +0545

    changed in settings v.2

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'firstProject.urls'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 TEMPLATES = [
     {

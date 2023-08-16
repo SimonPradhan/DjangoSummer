c79c4e3de032b1a045ef4aeb0e96fc87e3be40be
SimonPradhan <simon234pradhan@gmail.com>
2023-08-16 07:06:33 +0545

    new commit

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blogapplication-c9jr.onrender.com', 'simonpradhan.com.np']
+ALLOWED_HOSTS = ['127.0.0.1','blogapplication-c9jr.onrender.com', 'simonpradhan.com.np']
 
 
 # Application definition
